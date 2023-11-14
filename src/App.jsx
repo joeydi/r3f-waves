fbb1a6521aab1e6862fdbfe9ba7dff4c609774e0
Joe di Stefano <joeydi@okaypl.us>
2023-11-14 15:03:52 -0500

    remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,8 @@
 import { useRef, useState } from "react";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { Stats, OrbitControls, PointMaterial } from "@react-three/drei";
 import { useControls } from "leva";
 import "./styles/main.scss";
-
-function Box(props) {
-    // This reference will give us direct access to the mesh
-    const meshRef = useRef();
-    // Set up state for the hovered and active state
-    const [hovered, setHover] = useState(false);
-    const [active, setActive] = useState(false);
-    // Subscribe this component to the render-loop, rotate the mesh every frame
-    useFrame((state, delta) => (meshRef.current.rotation.x += delta));
-    // Return view, these are regular three.js elements expressed in JSX
-    return (
-        <mesh
-            {...props}
-            ref={meshRef}
-            scale={active ? 1.5 : 1}
-            onClick={(event) => setActive(!active)}
-            onPointerOver={(event) => setHover(true)}
-            onPointerOut={(event) => setHover(false)}>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-        </mesh>
-    );
-}
 
 function Waves() {
     const { width } = useControls({ width: 400 });
@@ -66,8 +43,6 @@ function Waves() {
 function App() {
     return (
         <Canvas camera={{ position: [0, 0, 150] }}>
-            {/* <ambientLight /> */}
-            {/* <pointLight position={[0, 0, 100]} intensity={30} color="#fff" /> */}
             <Waves />
             <OrbitControls />
             <Stats />
